a64e671ed8333b11dacbdbe51045141e728183c2
erffon <erffon@gmail.com>
2023-08-15 13:22:38 +0330

    Fix: Configs on default font size

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import { HEADER_DATA } from "../../../contents/layout/header/headerData";
 import Link from "next/link";
 import style from "./header.module.css";
+import Image from "next/image";
 
 interface IHeader {
   className?: string;
@@ -10,14 +10,15 @@ interface IHeader {
 const Header: React.FC<IHeader> = ({ className = "" }) => {
   return (
     <header className={`_container ${style["container"]} ${className}`}>
-      <div>
+      <Link href="/">
         <Image
           src={HEADER_DATA.english.logoImage}
           alt={"Iran's logo"}
           width={80}
           height={80}
         />
-      </div>
+      </Link>
+
       <ul className={`${style["ul"]} text-sm font-light`}>
         {HEADER_DATA.english.headerLinks.map((item) => {
           return (
